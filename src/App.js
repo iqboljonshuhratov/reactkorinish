a9b474a49f466eacf09a3a3c1473b59d12e65b8d
iqboljonshuhratov <shuhratjonoviqboljon79@gmail.com>
2023-05-21 16:42:17 +0500

    loyha

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <main class="Allbox">
+        <img class="All" src="https://images.wallpaperscraft.com/image/single/forest_fog_trees_132037_1366x768.jpg" alt=""/>
+        <div class="container">
+            <header class="header1">
+                <h1>K. Griffith</h1>
+                <ul>
+                    <li><a href="#">APPEARANCES</a></li>
+                        <li><a href="#">BOOKS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                </ul>
+            </header>
+            <div class="div1">
+                <p></p>
+            </div>
+    
+            <div class="nameBox">
+                <img src="https://static.wixstatic.com/media/ad420a_ca05081d4ece492fb6589c81baee1091~mv2.jpg/v1/crop/x_1416,y_0,w_3091,h_3414/fill/w_298,h_329,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-11967148821.jpg" alt="" />
+                <div>
+                    <h1>Kayla Griffith</h1>
+                    <span>Award Winning Author</span>
+                </div>
+            </div>
+    
+            <div class="info">
+                <div class="text">
+                    
+                    <p>New Release</p>
+                    <h1>The Swan Isle </h1>
+                    <h2>(2035)</h2>
+                    <span>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</span>
+                    <h3>Order Now</h3>
+    
+                    <ul>
+                        <li><button>Amazon</button></li>
+                        <li><button>gogle</button></li>
+                        <li><button>ibooks</button></li>
+                    </ul>
+                </div>
+                <div class="img">
+                    <img src="https://static.wixstatic.com/media/ad420a_ac6bbfd46dbe4c83a234221d29d67791~mv2.png/v1/fill/w_481,h_679,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Screen%20Shot%202021-02-07%20at%209_24_47.png" alt="" />
+                </div>
+            </div>
+    
+            <div class="infoTextBox">
+                <header>
+                    <h1>
+                        Praise & Reviews
+                    </h1>
+                </header>
+                <div class="textBox">
+                    <div class="text">
+                        <h1>“</h1>
+                        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                        <span>
+                            ‘Swan Isle’ is Griffith's best writing yet” The Times Book Review
+                        </span>
+    
+                    </div>
+                    <div class="text">
+                        <h1>“</h1>
+                        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                        <span>
+                            ‘Swan Isle’ is Griffith's best writing yet” The Times Book Review
+                        </span>
+    
+                    </div>
+                    <div class="text">
+                        <h1>“</h1>
+                        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                        <span>
+                            ‘Swan Isle’ is Griffith's best writing yet” The Times Book Review
+                        </span>
+    
+                    </div>
+                </div>
+            </div>
+        
+
+            <div class="JoinContainer">
+                <div class="text">
+                    <h1>See Upcoming Appearances</h1>
+                    <p>I'm a paragraph. Click here to add your own <br /> text and edit me. It's easy.</p>
+                </div>
+                <div class="join">
+                    <p>January 18th 2035, The Breakfast Club, Virtual Book Reading <br /> of Swan Isle, 7PM - 8PM EST</p>
+                    <button>Join</button>
+                </div>
+                <div class="join">
+                    <p>January 18th 2035, The Breakfast Club, Virtual Book Reading <br /> of Swan Isle, 7PM - 8PM EST</p>
+                    <button>Join</button>
+                </div>
+                <div class="join">
+                    <p>January 18th 2035, The Breakfast Club, Virtual Book Reading <br /> of Swan Isle, 7PM - 8PM EST</p>
+                    <button>Join</button>
+                </div>
+                
+            </div>
+
+            <div class="man">
+                <div class="manBox">
+                    <img src="https://static.wixstatic.com/media/ad420a_ca05081d4ece492fb6589c81baee1091~mv2.jpg/v1/crop/x_1416,y_0,w_3091,h_3414/fill/w_298,h_329,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-11967148821.jpg" alt=""/>
+                    <h1>About Kayla Griffith</h1>
+                    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                    <button>Read More</button>
+                </div>
+            </div>
+
+            <div class="futter">
+                © 2035 by K.Griffith. Powered and secured by Wix
+            </div>
+
+        </div>
+
+
+    </main>
+    </div>
+  );
+}
+
+export default App;
